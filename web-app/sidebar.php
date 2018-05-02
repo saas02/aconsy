ec699aacef8bcb136f85682bdd466938e3397bb6
sergio amaya <saas02@gmail.com>
2018-05-01 19:49:21 -0500

    actualizar estilos y url del sidebar

--- web-app/sidebar.php
+++ web-app/sidebar.php
@@ -3,23 +3,23 @@
     <div class="well sidebar-nav">
         <ul class="nav nav-list">
             <li>Men&uacute; Principal</li>
-            <li><a href="/ingreso/ingresoAutomatico.php">Ingreso de Personas</a></li>
-            <li><a href="/ingreso/ingresarElementoAutomatico.php">Ingreso de Elementos</a></li>
-            <li><a href="/ingreso/ingresar.php">Ingresos Manuales</a></li>
-            <li><a href="/reportes/generarReportes.php">Reportes</a></li>
+            <li><a href="/aconsy/ingreso/ingresoAutomatico.php">Ingreso de Personas</a></li>
+            <li><a href="/aconsy/ingreso/ingresarElementoAutomatico.php">Ingreso de Elementos</a></li>
+            <li><a href="/aconsy/ingreso/ingresar.php">Ingresos Manuales</a></li>
+            <li><a href="/aconsy/reportes/generarReportes.php">Reportes</a></li>
             <li class="nav-header">Registrar</li>
-            <li><a href="/elemento/registrarElementos.php">Elementos</a></li>
-            <li><a href="/vehiculo/registrarVehiculos.php">Vehiculos</a></li>
-            <li><a href="/usuario/buscar.php">Visitantes</a></li>
+            <li><a href="/aconsy/elemento/registrarElementos.php">Elementos</a></li>
+            <li><a href="/aconsy/vehiculo/registrarVehiculos.php">Vehiculos</a></li>
+            <li><a href="/aconsy/usuario/buscar.php">Visitantes</a></li>
             <li class="nav-header">Administrar</li>
-            <li><a href="/garaje/asignarGaraje.php">Garajes</a></li>
-            <li><a href="/curso/administarCurso.php">Cursos</a></li>
-            <li><a href="/usuario/administarUsuario.php">Usuarios</a></li>
-            <li><a href="/usuarioCurso/usuarioCurso.php">Clases</a></li>
-            <li><a href="/jornada/administrarJornada.php">Jornadas</a></li>
+            <li><a href="/aconsy/garaje/asignarGaraje.php">Garajes</a></li>
+            <li><a href="/aconsy/curso/administarCurso.php">Cursos</a></li>
+            <li><a href="/aconsy/usuario/administarUsuario.php">Usuarios</a></li>
+            <li><a href="/aconsy/usuarioCurso/usuarioCurso.php">Clases</a></li>
+            <li><a href="/aconsy/jornada/administrarJornada.php">Jornadas</a></li>
         </ul>
     </div><!--/.well -->
-    <img src="/web-app/img/aconsysinfondo.png" />
+    <img src="/aconsy/web-app/img/aconsysinfondo.png" />
 </div><!--/span-->
 <div class="span9"><!--inicio columna derecha-->
 
